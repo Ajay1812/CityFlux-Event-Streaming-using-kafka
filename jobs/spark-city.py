7144839a30e9c1bab6a8f1cc296ef6537ce8c70d
Ajay1812 <a.kumar01c@gmail.com>
2025-04-07 15:19:30 +0530

    read kafka topic and parquet file stored in s3

--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -1,13 +1,14 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StructField, StringType, TimestampType, DoubleType, IntegerType
+from pyspark.sql.functions import from_json, col
 from config import configuration
 
 def main():
     spark = SparkSession.builder.appName("CityFlux-streaming")\
     .config("spark.jars.packages", 
-            "org.apache.spark:spark-streaming-kafka-0-10_2.13:3.5.0,"
+             "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,"
             "org.apache.hadoop:hadoop-aws:3.3.1,"
-            "com.amazonaws:aws-java-sdk:1.11.469,")\
+            "com.amazonaws:aws-java-sdk:1.11.469") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
     .config("spark.hadoop.fs.s3a.access.key", configuration.get('AWS_ACCESS_KEY'))\
     .config("spark.hadoop.fs.s3a.secret.key", configuration.get('AWS_SECRET_KEY'))\
@@ -78,7 +79,53 @@ def main():
         StructField("description", StringType(), True),
     ])
 
-    vehicleDF = read_kakfa_topic('vehicle_data', vehicleSchema).alias('vehicle')
+    def read_kakfa_topic(topic, schema):
+        return (spark.readStream
+                .format('kafka')
+                .option('kafka.bootstrap.servers', 'broker:29092')
+                .option('subscribe', topic)
+                .option('startingOffset', 'earliest')
+                .load()
+                .selectExpr('CAST(value AS STRING)')
+                .select(from_json(col('value'), schema).alias('data'))
+                .select('data.*')
+                .withWatermark('timestamp','5 minutes')
+                )
 
+    def streamWriter(input : DataFrame, checkpointFolder, output):
+        return (input.writeStream
+                .format('parquet')
+                .option('checkPointLocation', checkpointFolder)
+                .option('path', output)
+                .outputMode('append')
+                .start())
+
+    vehicleDF = read_kakfa_topic('vehicle_data', vehicleSchema).alias('vehicle')
+    gpsDF = read_kakfa_topic('gps_data', vehicleSchema).alias('gps')
+    trafficDF = read_kakfa_topic('traffic_data', vehicleSchema).alias('traffic')
+    weatherDF = read_kakfa_topic('weather_data', vehicleSchema).alias('weather')
+    emergencyDF = read_kakfa_topic('emergency_data', vehicleSchema).alias('emergency')
+
+    # Join all dfs with id and timestamp col
+    # joinedDf
+
+    query1 = streamWriter(vehicleDF, 
+                 f's3a://{configuration.get('S3_BUCKET')}/checkpoints/vehicle_data',
+                 f's3a://{configuration.get('S3_BUCKET')}/data/vehicle_data')
+    query2 = streamWriter(gpsDF, 
+                 f's3a://{configuration.get('S3_BUCKET')}/checkpoints/gps_data',
+                 f's3a://{configuration.get('S3_BUCKET')}/data/gps_data')
+    query3 = streamWriter(trafficDF, 
+                 f's3a://{configuration.get('S3_BUCKET')}/checkpoints/traffic_data',
+                 f's3a://{configuration.get('S3_BUCKET')}/data/traffic_data')
+    query4 = streamWriter(weatherDF, 
+                 f's3a://{configuration.get('S3_BUCKET')}/checkpoints/weather_data',
+                 f's3a://{configuration.get('S3_BUCKET')}/data/weather_data')
+    query5 = streamWriter(emergencyDF, 
+                 f's3a://{configuration.get('S3_BUCKET')}/checkpoints/emergency_data',
+                 f's3a://{configuration.get('S3_BUCKET')}/data/emergency_data')
+
+
+    query5.awaitTermination()
 if __name__ == "__main__":
     main()
